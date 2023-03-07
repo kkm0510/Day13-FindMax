5d92313c1857268d0cb4adb0fededce9518ad1f3
kkm0510 <kartikeya.maan1997@yahoo.com>
2023-03-07 10:59:47 +0530

    ADDED : code to find max of 3 Integers

--- src/TestMax.java
+++ src/TestMax.java
@@ -1,2 +1,15 @@
 public class TestMax {
+
+    public static Integer findMax(Integer a, Integer b, Integer c){
+        Integer max=a;
+        if(b.compareTo(max)>0) max=b;
+        if(c.compareTo(max)>0) max=c;
+        return max;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("TC-1.1 : " + findMax(4, 2, 3));
+        System.out.println("TC-1.2 : " + findMax(2, 4, 3));
+        System.out.println("TC-1.3 : " + findMax(2, 3, 4));
+    }
 }
