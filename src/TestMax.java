3eb1f37b7941b2434dbc4852e2051be84b536e5d
kkm0510 <kartikeya.maan1997@yahoo.com>
2023-03-07 11:05:19 +0530

    ADDED : code to find max of 3 floats

--- src/TestMax.java
+++ src/TestMax.java
@@ -7,9 +7,19 @@ public class TestMax {
         return max;
     }
 
+    public static Float findMax(Float a, Float b, Float c){
+        float max=a;
+        if(b.compareTo(max)>0) max=b;
+        if(c.compareTo(max)>0) max=c;
+        return max;
+    }
+
     public static void main(String[] args) {
         System.out.println("TC-1.1 : " + findMax(4, 2, 3));
         System.out.println("TC-1.2 : " + findMax(2, 4, 3));
         System.out.println("TC-1.3 : " + findMax(2, 3, 4));
+        System.out.println("TC-2.1 : " + findMax(4.0f, 2.0f, 3.0f));
+        System.out.println("TC-2.2 : " + findMax(2.0f, 4.0f, 3.0f));
+        System.out.println("TC-2.3 : " + findMax(2.0f, 3.0f, 4.0f));
     }
 }
