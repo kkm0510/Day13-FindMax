ed58cede923b9db3e3d8df18b8afa5b00a6f189b
kkm0510 <kartikeya.maan1997@yahoo.com>
2023-03-07 11:43:47 +0530

    REFACTORED : all findMax() to one generic method

--- src/TestMax.java
+++ src/TestMax.java
@@ -1,6 +1,6 @@
-public class TestMax <T extends Comparable<T>>{
+public class TestMax {
 
-    public T findMax(T a, T b, T c){
+    public static <T extends Comparable<T>> T findMax(T a, T b, T c){
         T max=a;
         if(b.compareTo(max)>0) max=b;
         if(c.compareTo(max)>0) max=c;
@@ -9,17 +9,14 @@ public class TestMax <T extends Comparable<T>>{
 
     public static void main(String[] args) {
 
-        TestMax<Integer> maxInteger=new TestMax<>();
-        TestMax<Float> maxFloat=new TestMax<>();
-        TestMax<String> maxString=new TestMax<>();
-        System.out.println("TC-1.1 : " + maxInteger.findMax(4, 2, 3));
-        System.out.println("TC-1.2 : " + maxInteger.findMax(2, 4, 3));
-        System.out.println("TC-1.3 : " + maxInteger.findMax(2, 3, 4));
-        System.out.println("TC-2.1 : " + maxFloat.findMax(4.0f, 2.0f, 3.0f));
-        System.out.println("TC-2.2 : " + maxFloat.findMax(2.0f, 4.0f, 3.0f));
-        System.out.println("TC-2.3 : " + maxFloat.findMax(2.0f, 3.0f, 4.0f));
-        System.out.println("TC-3.1 : " + maxString.findMax("Apple", "Peach", "Banana"));
-        System.out.println("TC-3.2 : " + maxString.findMax("Apple", "Peach", "Banana"));
-        System.out.println("TC-3.3 : " + maxString.findMax("Apple", "Peach", "Banana"));
+        System.out.println("TC-1.1 : " + findMax(4, 2, 3));
+        System.out.println("TC-1.2 : " + findMax(2, 4, 3));
+        System.out.println("TC-1.3 : " + findMax(2, 3, 4));
+        System.out.println("TC-2.1 : " + findMax(4.0f, 2.0f, 3.0f));
+        System.out.println("TC-2.2 : " + findMax(2.0f, 4.0f, 3.0f));
+        System.out.println("TC-2.3 : " + findMax(2.0f, 3.0f, 4.0f));
+        System.out.println("TC-3.1 : " + findMax("Apple", "Peach", "Banana"));
+        System.out.println("TC-3.2 : " + findMax("Apple", "Peach", "Banana"));
+        System.out.println("TC-3.3 : " + findMax("Apple", "Peach", "Banana"));
     }
 }
