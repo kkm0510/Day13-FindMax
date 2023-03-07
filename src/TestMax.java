1e22e337e3b64ba37adfd9fa1dbbcb2e1616ca8f
kkm0510 <kartikeya.maan1997@yahoo.com>
2023-03-07 12:07:06 +0530

    REFACTORED : TestMax class to generic class

--- src/TestMax.java
+++ src/TestMax.java
@@ -23,17 +23,17 @@ public class TestMax <E extends Comparable<E>>{
     }
 
     public static void main(String[] args) {
-        new TestMax<Integer>(4, 2, 3).testMaximum();
-        new TestMax<Integer>(2, 4, 3).testMaximum();
-        new TestMax<Integer>(2, 3, 4).testMaximum();
+        new TestMax<>(4, 2, 3).testMaximum();
+        new TestMax<>(2, 4, 3).testMaximum();
+        new TestMax<>(2, 3, 4).testMaximum();
 
-        new TestMax<Float>(4.0f, 2.0f, 3.0f).testMaximum();
-        new TestMax<Float>(2.0f, 4.0f, 3.0f).testMaximum();
-        new TestMax<Float>(2.0f, 3.0f, 4.0f).testMaximum();
+        new TestMax<>(4.0f, 2.0f, 3.0f).testMaximum();
+        new TestMax<>(2.0f, 4.0f, 3.0f).testMaximum();
+        new TestMax<>(2.0f, 3.0f, 4.0f).testMaximum();
 
-        new TestMax<String>("Apple", "Banana", "Peach").testMaximum();
-        new TestMax<String>("Banana", "Apple", "Peach").testMaximum();
-        new TestMax<String>("Banana", "Peach", "Apple").testMaximum();
+        new TestMax<>("Apple", "Banana", "Peach").testMaximum();
+        new TestMax<>("Banana", "Apple", "Peach").testMaximum();
+        new TestMax<>("Banana", "Peach", "Apple").testMaximum();
 
     }
 }
