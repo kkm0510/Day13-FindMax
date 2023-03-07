63704dcb2632505df3aeb900d1b9f0b7d6b210d4
kkm0510 <kartikeya.maan1997@yahoo.com>
2023-03-07 11:23:36 +0530

    REFACTORED : all findMax() to one generic method

--- src/TestMax.java
+++ src/TestMax.java
@@ -1,35 +1,25 @@
-public class TestMax {
+public class TestMax <T extends Comparable<T>>{
 
-    public static Integer findMax(Integer a, Integer b, Integer c){
-        Integer max=a;
-        if(b.compareTo(max)>0) max=b;
-        if(c.compareTo(max)>0) max=c;
-        return max;
-    }
-
-    public static Float findMax(Float a, Float b, Float c){
-        float max=a;
-        if(b.compareTo(max)>0) max=b;
-        if(c.compareTo(max)>0) max=c;
-        return max;
-    }
-
-    public static String findMax(String a, String b, String c){
-        String max=a;
+    public T findMax(T a, T b, T c){
+        T max=a;
         if(b.compareTo(max)>0) max=b;
         if(c.compareTo(max)>0) max=c;
         return max;
     }
 
     public static void main(String[] args) {
-        System.out.println("TC-1.1 : " + findMax(4, 2, 3));
-        System.out.println("TC-1.2 : " + findMax(2, 4, 3));
-        System.out.println("TC-1.3 : " + findMax(2, 3, 4));
-        System.out.println("TC-2.1 : " + findMax(4.0f, 2.0f, 3.0f));
-        System.out.println("TC-2.2 : " + findMax(2.0f, 4.0f, 3.0f));
-        System.out.println("TC-2.3 : " + findMax(2.0f, 3.0f, 4.0f));
-        System.out.println("TC-3.1 : " + findMax("Apple", "Peach", "Banana"));
-        System.out.println("TC-3.2 : " + findMax("Apple", "Peach", "Banana"));
-        System.out.println("TC-3.3 : " + findMax("Apple", "Peach", "Banana"));
+
+        TestMax<Integer> maxInteger=new TestMax<>();
+        TestMax<Float> maxFloat=new TestMax<>();
+        TestMax<String> maxString=new TestMax<>();
+        System.out.println("TC-1.1 : " + maxInteger.findMax(4, 2, 3));
+        System.out.println("TC-1.2 : " + maxInteger.findMax(2, 4, 3));
+        System.out.println("TC-1.3 : " + maxInteger.findMax(2, 3, 4));
+        System.out.println("TC-2.1 : " + maxFloat.findMax(4.0f, 2.0f, 3.0f));
+        System.out.println("TC-2.2 : " + maxFloat.findMax(2.0f, 4.0f, 3.0f));
+        System.out.println("TC-2.3 : " + maxFloat.findMax(2.0f, 3.0f, 4.0f));
+        System.out.println("TC-3.1 : " + maxString.findMax("Apple", "Peach", "Banana"));
+        System.out.println("TC-3.2 : " + maxString.findMax("Apple", "Peach", "Banana"));
+        System.out.println("TC-3.3 : " + maxString.findMax("Apple", "Peach", "Banana"));
     }
 }
